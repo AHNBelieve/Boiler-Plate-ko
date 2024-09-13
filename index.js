9a87f2e9cb94418e488123562eb9e3dcb857cf51
AHNBelieve <an0731an@cau.ac.kr>
2024-09-13 10:00:25 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://an0731an:dks123ss0219ss@boilerplate0.anvxt.mongodb.net/?retryWrites=true&w=majority&appName=BoilerPlate0"
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log("mongoDB Connected");
   })
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("hello World!");
+  res.send("hello World!asdsa");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 할 때 필요한 정보를 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어줌
+
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
