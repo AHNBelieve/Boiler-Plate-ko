6ee0d6e23be5ceba3ee65cb0ee305abb285e4b87
AHNBelieve <an0731an@cau.ac.kr>
2024-09-20 10:27:05 +0900

    auth와 로그아웃 기능 구현 완료

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("hello World!asdsa");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   //회원가입 할 때 필요한 정보를 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어줌
 
   const user = new User(req.body);
@@ -43,7 +44,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 이메일이 DB에 있는지 확인
   User.findOne({
     email: req.body.email,
@@ -75,6 +76,42 @@ app.post("/login", (req, res) => {
       console.log(err);
       return res.status(400).json({
         loginSuccess: false,
+        message: err.message,
+      });
+    });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기 까지 middleware를 통과해 왔다는 이야기는 곧 Authentication이 true라는 말!
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    {
+      _id: req.user._id,
+    },
+    {
+      token: "",
+    }
+  )
+    .then(() => {
+      return res.status(200).json({
+        logoutSuccess: true,
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        logoutSuccess: false,
         message: err.message,
       });
     });
